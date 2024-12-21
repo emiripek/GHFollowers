1e5684a42376f94eb7534a861fb71f104894f561
Emirhan İpek <emirrhanipek@gmail.com>
2024-12-21 17:19:45 +0300

    SearchVC & Custom Alert

---
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Emirhan İpek on 15.12.2024.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
