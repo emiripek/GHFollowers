cb31350182fa8d3c3455c638c3656926f6808867
Emirhan İpek <emirrhanipek@gmail.com>
2024-12-22 18:35:44 +0300

    Completed Networking

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Emirhan İpek on 22.12.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
+
